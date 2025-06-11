67b24973fcbb1b88f6fa9ed4107e22cb2e505bec
tushar4408 <tusharjha7483@gmail.com>
2025-06-11 21:51:48 +0530

    Create create_students.sql

---
+++ create_students.sql
@@ -0,0 +1,11 @@
+-- Create table and insert some data
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    grade VARCHAR(10)
+);
+
+INSERT INTO students VALUES
+(1, 'Tushar Jha', 'A'),
+(2, 'Riya Sharma', 'B'),
+(3, 'Raj Gupta', 'A');
